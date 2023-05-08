e215a0118e9180d9d71365b077bdeb87ba03f54f
iamwithtech <iamwith.tech@gmail.com>
2023-05-08 18:01:03 +0530

    Added Configuration, Discovery, Gateway and user application.

---
+++ user-api/src/test/java/com/beyondctc/app/userapi/ApplicationUserApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beyondctc.app.userapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationUserApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
