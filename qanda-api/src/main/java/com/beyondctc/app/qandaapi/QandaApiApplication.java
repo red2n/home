b3fd940a7de8aa000e8a784ba99e0b63dc52c096
iamwithtech <iamwith.tech@gmail.com>
2023-05-08 18:43:12 +0530

    added docker-compose.yml file also added Zipkin container and test the API call log.

---
+++ qanda-api/src/main/java/com/beyondctc/app/qandaapi/QandaApiApplication.java
@@ -0,0 +1,13 @@
+package com.beyondctc.app.qandaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QandaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QandaApiApplication.class, args);
+	}
+
+}
---
+++ qanda-api/src/test/java/com/beyondctc/app/qandaapi/QandaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beyondctc.app.qandaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QandaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
